622886115f5312ebafb1c5d364ec33d676c5caaf
saramforte <saraforte310@gmail.com>
2025-05-09 10:00:38 +0200

    Added to the proyect Spring with Thymeleaf and dependecies

---
+++ server/src/main/java/com/knowy/server/controller/PresentationController.java
@@ -0,0 +1,13 @@
+package com.knowy.server.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class PresentationController {
+
+	@GetMapping("/")
+	public String viewLandingPage() {
+		return "pages/example";
+	}
+}
