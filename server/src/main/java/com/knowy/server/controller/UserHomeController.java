c617b687803303e0c41ecb482da051334cc8c4c9
saramforte <saraforte310@gmail.com>
2025-05-29 08:32:56 +0200

    Created new carousel of news and integrated with thymeleaf

--- server/src/main/java/com/knowy/server/controller/UserHomeController.java
+++ server/src/main/java/com/knowy/server/controller/UserHomeController.java
@@ -1,5 +1,6 @@
 package com.knowy.server.controller;
 
+import com.knowy.server.controller.model.NewsHomeDto;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -11,7 +12,25 @@ import java.util.List;
 public class UserHomeController {
 
 	@GetMapping("/home")
-	public String userHome() {
+	public String userHome(Model model) {
+		List<NewsHomeDto> newsHome = new ArrayList<>();
+		newsHome.add(new NewsHomeDto(1, "La conspiración de los grillos", "Una historia que no deja dormir",
+			"https://placehold.org/900x900/ff6f61?text=900x900", "https://placehold.org/900x900/ff6f61?text=900x900"));
+
+		newsHome.add(new NewsHomeDto(2, "El código de las sombras", "Tecnología, misterio y traición",
+			"https://placehold.org/900x900/6b5b95?text=900x900", "https://placehold.org/900x900/6b5b95?text=900x900"));
+
+		newsHome.add(new NewsHomeDto(3, "Viaje al centro del café", "Una travesía entre aromas y secretos",
+			"https://placehold.org/900x900/88b04b?text=900x900", "https://placehold.org/900x900/88b04b?text=900x900"));
+
+		newsHome.add(new NewsHomeDto(4, "No abras la puerta roja", "Suspenso psicológico en cada página",
+			"https://placehold.org/900x900/d65076?text=900x900", "https://placehold.org/900x900/d65076?text=900x900"));
+
+		newsHome.add(new NewsHomeDto(5, "La física del silencio", "Cuando el universo conspira para callar",
+			"https://placehold.org/900x900/45b8ac?text=900x900", "https://placehold.org/900x900/45b8ac?text=900x900"));
+
+
+		model.addAttribute("newsHome", newsHome);
 		return "pages/user-home";
 	}
 
--- server/src/main/java/com/knowy/server/controller/model/NewsHomeDto.java
+++ server/src/main/java/com/knowy/server/controller/model/NewsHomeDto.java
@@ -1,23 +1,4 @@
 package com.knowy.server.controller.model;
 
-import lombok.Getter;
-import lombok.Setter;
-
-@Getter
-@Setter
-public class NewsHomeDto {
-
-	private int id;
-	private String title;
-	private String description;
-	private String image;
-	private String url;
-
-	public NewsHomeDto(int id, String title, String description, String image, String url) {
-		this.id = id;
-		this.title = title;
-		this.description = description;
-		this.image = image;
-		this.url = url;
-	}
+public record NewsHomeDto(int id, String title, String description, String imageUrl, String linkUrl) {
 }
