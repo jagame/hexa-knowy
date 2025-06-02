2803938c1d198c5a1c161ca556199c3ab0707bc6
Worldlover12 <manumk.097@gmail.com>
2025-06-02 18:48:56 +0200

    Merge remote-tracking branch 'origin/KNOWY-78' into KNOWY-78

--- server/src/main/java/com/knowy/server/controller/MissionsController.java
+++ server/src/main/java/com/knowy/server/controller/MissionsController.java
@@ -1,22 +1,18 @@
 package com.knowy.server.controller;
 
-import com.knowy.server.controller.model.NewsDto;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 
-import java.util.ArrayList;
-import java.util.List;
-
 @Controller
-public class StreakController {
-	@GetMapping("/Streak")
-	public String StreakCardTry(ModelMap interfaceScreen) { //estos atributos de controladore serían en la futura página de usuario
+public class MissionsController {
+	@GetMapping("/Test")
+	public String MissionsCtrl(ModelMap interfaceScreen) { //estos atributos de controladore serían en la futura página de usuario
 		interfaceScreen.addAttribute("leccionesCompletadas", 20);
 		interfaceScreen.addAttribute("progresoPorcentaje2", 20);
 		interfaceScreen.addAttribute("leccionesTotales", 20);
 		interfaceScreen.addAttribute("linkRetoDiario", "/");
 		interfaceScreen.addAttribute("rachaActual2", 10);
-		return "pages/streakCardTest";
+		return "Test";
 	}
 }
