b1e9e11dc6ce44bcf607402fdcc4b5a2e77eab0f
Worldlover12 <manumk.097@gmail.com>
2025-06-03 09:00:47 +0200

    Merge remote-tracking branch 'origin/KNOWY-78' into KNOWY-78

--- server/src/main/java/com/knowy/server/controller/MissionsController.java
+++ server/src/main/java/com/knowy/server/controller/MissionsController.java
@@ -1,18 +1,1 @@
-package com.knowy.server.controller;
 
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.ModelMap;
-import org.springframework.web.bind.annotation.GetMapping;
-
-@Controller
-public class MissionsController {
-	@GetMapping("/Test")
-	public String MissionsCtrl(ModelMap interfaceScreen) { //estos atributos de controladore serían en la futura página de usuario
-		interfaceScreen.addAttribute("leccionesCompletadas", 20);
-		interfaceScreen.addAttribute("progresoPorcentaje2", 20);
-		interfaceScreen.addAttribute("leccionesTotales", 20);
-		interfaceScreen.addAttribute("linkRetoDiario", "/");
-		interfaceScreen.addAttribute("rachaActual2", 10);
-		return "Test";
-	}
-}
--- server/src/main/java/com/knowy/server/controller/UserHomeController.java
+++ server/src/main/java/com/knowy/server/controller/UserHomeController.java
@@ -1,7 +1,13 @@
 package com.knowy.server.controller;
 
+import com.knowy.server.controller.model.MissionsDto;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Controller
 public class UserHomeController {
