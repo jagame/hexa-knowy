9cb1b6dc6905f3422b3f9ed16e0f63b4f21011a3
Sinnick29 <davidgilcampos@gmail.com>
2025-06-04 18:25:18 +0200

    I have adapted the user management pages to the new aside template. In addition, I have loaded the links to the bootstrap icons and so on in the new aside template for the functioning of the page. Deleted the previous pages, as well as the old account-sidebar, as it is no longer needed.

--- server/src/main/java/com/knowy/server/controller/UserConfigController.java
+++ server/src/main/java/com/knowy/server/controller/UserConfigController.java
@@ -6,17 +6,18 @@ import org.springframework.web.bind.annotation.*;
 
 @Controller
 public class UserConfigController {
+	String username = "usuario123";
 	//User-Profile
 	@GetMapping("/user-profile")
 	public String viewUserProfile(ModelMap interfaceScreen) {
-		interfaceScreen.addAttribute("username", "usuario123");
+		interfaceScreen.addAttribute("username", username);
 		return "pages/user-management/user-profile";
 	}
 
 	//User-Account
 	@GetMapping("/user-account")
 	public String viewUserAccount(ModelMap interfaceScreen) {
-		interfaceScreen.addAttribute("username", "usuario123");
+		interfaceScreen.addAttribute("username", username);
 		interfaceScreen.addAttribute("email", "usuario123@correo.com");
 		return "pages/user-management/user-account";
 	}
@@ -24,14 +25,14 @@ public class UserConfigController {
 	// Delete account
 	@GetMapping("/delete-account")
 	public String deleteAccountForm(ModelMap interfaceScreen) {
-		interfaceScreen.addAttribute("username", "usuario123");
+		interfaceScreen.addAttribute("username", username);
 		return "pages/user-management/delete-account";
 	}
 
 	//Delete-Account-End (Finally deleting Account)
 	@GetMapping ("/delete-account-end")
 	public String deleteAccountEnd(ModelMap interfaceScreen) {
-		interfaceScreen.addAttribute("username", "usuario123");
+		interfaceScreen.addAttribute("username", username);
 		return "pages/user-management/delete-account-end";
 	}
 }
