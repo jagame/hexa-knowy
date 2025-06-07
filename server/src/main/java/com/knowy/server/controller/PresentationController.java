d1b202af572e289e8018b005c1d7177036547138
ivancarvajalhuetor <ivancarvajalhuetor@gmail.com>
2025-06-07 13:51:26 +0200

    replaced bootstrap accordion with html accordion. updated css.

--- server/src/main/java/com/knowy/server/controller/PresentationController.java
+++ server/src/main/java/com/knowy/server/controller/PresentationController.java
@@ -45,6 +45,7 @@ public class PresentationController {
 		List<SolutionDto> solutions = new ArrayList<>();
 		solutions.add(new SolutionDto("Tarjeta 1: JavaScript vs Java", "Pregunta tarjeta 1", "Solución tarjeta 1"));
 		solutions.add(new SolutionDto("Tarjeta 2: PHP", "Pregunta tarjeta 2", "Solución tarjeta 2"));
+		solutions.add(new SolutionDto("Tarjeta 3: Python", "Pregunta tarjeta 3", "Solución tarjeta 3"));
 		interfaceScreen.addAttribute("solutions", solutions);
 
 		return "/pages/landing-page";
