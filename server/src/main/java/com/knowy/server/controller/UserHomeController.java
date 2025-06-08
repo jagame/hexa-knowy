0e6a01edbd030b0652816e9dd04f7bdd7f587353
saramforte <saraforte310@gmail.com>
2025-06-08 09:27:28 +0200

    Styled carousel news

--- server/src/main/java/com/knowy/server/controller/UserHomeController.java
+++ server/src/main/java/com/knowy/server/controller/UserHomeController.java
@@ -15,19 +15,19 @@ public class UserHomeController {
 	public String userHome(Model model) {
 		List<NewsHomeDto> newsHome = new ArrayList<>();
 		newsHome.add(new NewsHomeDto(1, "La conspiración de los grillos", "Una historia que no deja dormir",
-			"https://placehold.org/900x900/ff6f61?text=900x900", "https://placehold.org/900x900/ff6f61?text=900x900"));
+			"https://picsum.photos/id/10/900/900", "https://picsum.photos/id/10/900/900"));
 
 		newsHome.add(new NewsHomeDto(2, "El código de las sombras", "Tecnología, misterio y traición",
-			"https://placehold.org/900x900/6b5b95?text=900x900", "https://placehold.org/900x900/6b5b95?text=900x900"));
+			"https://picsum.photos/id/20/900/900", "https://picsum.photos/id/20/900/900"));
 
 		newsHome.add(new NewsHomeDto(3, "Viaje al centro del café", "Una travesía entre aromas y secretos",
-			"https://placehold.org/900x900/88b04b?text=900x900", "https://placehold.org/900x900/88b04b?text=900x900"));
+			"https://picsum.photos/id/30/900/900", "https://picsum.photos/id/30/900/900"));
 
 		newsHome.add(new NewsHomeDto(4, "No abras la puerta roja", "Suspenso psicológico en cada página",
-			"https://placehold.org/900x900/d65076?text=900x900", "https://placehold.org/900x900/d65076?text=900x900"));
+			"https://picsum.photos/id/40/900/900", "https://picsum.photos/id/40/900/900"));
 
 		newsHome.add(new NewsHomeDto(5, "La física del silencio", "Cuando el universo conspira para callar",
-			"https://placehold.org/900x900/45b8ac?text=900x900", "https://placehold.org/900x900/45b8ac?text=900x900"));
+			"https://picsum.photos/id/50/900/900", "https://picsum.photos/id/50/900/900"));
 
 
 		model.addAttribute("newsHome", newsHome);
