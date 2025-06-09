9f5ca0f40c5ba4593a8b62d634ca62202e6932ce
Worldlover12 <manumk.097@gmail.com>
2025-06-09 21:09:36 +0200

    Merge remote-tracking branch 'origin/KNOWY-78' into KNOWY-78

--- server/src/main/java/com/knowy/server/controller/modalController.java
+++ server/src/main/java/com/knowy/server/controller/modalController.java
@@ -1,21 +1,21 @@
-package com.knowy.server.controller;
-
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.servlet.mvc.support.RedirectAttributes;
-
-@Controller
-public class modalController {
-
-	private static String ultimaDificultadSeleccionada = "No seleccionada";
-
-
-	@PostMapping("/feedback/submit")
-	public String submitEval(@RequestParam("dificultad") String dificultad, RedirectAttributes redirectAttributes) {
-		System.out.println("Dificultad seleccionada por el usuario: " + dificultad);
-		ultimaDificultadSeleccionada = dificultad;
-		redirectAttributes.addFlashAttribute("mensajeFeedback", "Gracias por tu feedback: " + dificultad);
-		return "redirect:/";
-	}
-}
+//package com.knowy.server.controller;
+//
+//import org.springframework.stereotype.Controller;
+//import org.springframework.web.bind.annotation.PostMapping;
+//import org.springframework.web.bind.annotation.RequestParam;
+//import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+//
+//@Controller
+//public class modalController {
+//
+//	private static int valoracion;
+//
+//
+//	@PostMapping("/feedback/submit")
+//	public String submitEval(@RequestParam("dificultad") String dificultad, RedirectAttributes redirectAttributes) {
+//		System.out.println("Dificultad seleccionada por el usuario: " + dificultad);
+//		valoracion = Integer.parseInt(dificultad); //aquí invocaríamos un metodo de srvc para luego almacenar. Hago DTO?
+//		redirectAttributes.addFlashAttribute("mensajeFeedback", "Gracias por tu feedback: " + dificultad);
+//		return "redirect:/";
+//	}
+//}
