46baff721f685010c78bf0b59209b0214b07ba4f
Nicolás Richards Fernández <nrichardsf@gmail.com>
2025-06-10 14:50:19 +0200

    Lessons component.

--- server/src/main/java/com/knowy/server/controller/CoursePage.java
+++ server/src/main/java/com/knowy/server/controller/CoursePage.java
@@ -9,13 +9,13 @@ import org.springframework.web.bind.annotation.GetMapping;
 import java.util.List;
 
 @Controller
-public class CursoMockController {
+public class CoursePage {
 
 	@GetMapping("/course")
 	public String mostrarCurso(Model model) {
 		CursoDTO curso = new CursoDTO();
 		curso.setNombre("Java Básico");
-		curso.setPorcentajeCompletado(80);
+		curso.setPorcentajeCompletado(40);
 
 		List<LeccionDTO> lecciones = List.of(
 			new LeccionDTO(1, "Introducción", LeccionDTO.EstadoLeccion.COMPLETADA),
@@ -25,11 +25,19 @@ public class CursoMockController {
 			new LeccionDTO(5, "Bucles", LeccionDTO.EstadoLeccion.COMPLETADA),
 			new LeccionDTO(6, "Métodos (funciones)", LeccionDTO.EstadoLeccion.COMPLETADA),
 			new LeccionDTO(7, "Arreglos (arrays)", LeccionDTO.EstadoLeccion.SIGUIENTE),
-			new LeccionDTO(8, "Introducción ", LeccionDTO.EstadoLeccion.BLOQUEADA)
+			new LeccionDTO(8, "Introducción a la Programación Orientada a Objetos", LeccionDTO.EstadoLeccion.BLOQUEADA),
+			new LeccionDTO(9, "Introducción a la Programación Orientada a Objetos", LeccionDTO.EstadoLeccion.BLOQUEADA),
+			new LeccionDTO(10, "Introducción a la Programación Orientada a Objetos", LeccionDTO.EstadoLeccion.BLOQUEADA)
 		);
-
+		int lastLesson = -1;
+		for(int i = 0; i < lecciones.size(); i++) {
+			if(lecciones.get(i).getEstado() == LeccionDTO.EstadoLeccion.COMPLETADA) {
+				lastLesson = i;
+			}
+		}
 		curso.setLecciones(lecciones);
 		model.addAttribute("curso", curso);
+		model.addAttribute("lastLesson", lastLesson);
 		return "/pages/course-page";
 	}
 }
