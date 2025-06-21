62c42e4566a1e5944675c53efa6fcafc4279cd0c
saramforte <saraforte310@gmail.com>
2025-06-21 11:53:43 +0200

    Fixed create table; Insert dummy data of private and public user tables

--- sql/init/00-create-table.sql
+++ sql/init/00-create-table.sql
@@ -1,17 +1,15 @@
-BEGIN;
-
-
-CREATE TABLE IF NOT EXISTS public.private_user
-(
-    id serial NOT NULL,
-    id_public_user integer NOT NULL,
-    PRIMARY KEY (id),
-    UNIQUE (id_public_user)
-);
-
 CREATE TABLE IF NOT EXISTS public.public_user
 (
     id serial NOT NULL,
+	nickname varchar(50) NOT NULL UNIQUE,
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE IF NOT EXISTS public.private_user
+(
+    id integer NOT NULL,
+	email varchar(100) NOT NULL UNIQUE,
+	password varchar(100) NOT NULL,
     PRIMARY KEY (id)
 );
 
@@ -32,7 +30,7 @@ CREATE TABLE IF NOT EXISTS public.public_user_lesson
 (
     id_public_user integer NOT NULL,
     id_lesson integer NOT NULL,
-    PRIMARY KEY (id_public_user, id_course)
+    PRIMARY KEY (id_public_user, id_lesson)
 );
 
 CREATE TABLE IF NOT EXISTS public.course
@@ -63,7 +61,7 @@ CREATE TABLE IF NOT EXISTS public.option
 (
     id serial NOT NULL,
     id_excercise integer NOT NULL,
-    PRIMARY KEY (id, id_excercise)
+    PRIMARY KEY (id)
 );
 
 CREATE TABLE IF NOT EXISTS public.public_user_option
@@ -73,99 +71,53 @@ CREATE TABLE IF NOT EXISTS public.public_user_option
     PRIMARY KEY (id_public_user, id_option)
 );
 
+-- FK private_user
 ALTER TABLE IF EXISTS public.private_user
-    ADD FOREIGN KEY (id_public_user)
-    REFERENCES public.public_user (id) MATCH SIMPLE
-    ON UPDATE NO ACTION
-    ON DELETE NO ACTION
-    NOT VALID;
-
-
-ALTER TABLE IF EXISTS public.public_user
     ADD FOREIGN KEY (id)
-    REFERENCES public.public_user_mission (id_public_user) MATCH SIMPLE
-    ON UPDATE NO ACTION
-    ON DELETE NO ACTION
-    NOT VALID;
+    REFERENCES public.public_user (id);
 
-
+-- FK public_user_mission
 ALTER TABLE IF EXISTS public.public_user_mission
     ADD FOREIGN KEY (id_public_user)
-    REFERENCES public.public_user (id) MATCH SIMPLE
-    ON UPDATE NO ACTION
-    ON DELETE NO ACTION
-    NOT VALID;
-
+    REFERENCES public.public_user (id);
 
 ALTER TABLE IF EXISTS public.public_user_mission
     ADD FOREIGN KEY (id_mission)
-    REFERENCES public.mission (id) MATCH SIMPLE
-    ON UPDATE NO ACTION
-    ON DELETE NO ACTION
-    NOT VALID;
+    REFERENCES public.mission (id);
 
-
+-- FK public_user_lesson
 ALTER TABLE IF EXISTS public.public_user_lesson
     ADD FOREIGN KEY (id_public_user)
-    REFERENCES public.public_user (id) MATCH SIMPLE
-    ON UPDATE NO ACTION
-    ON DELETE NO ACTION
-    NOT VALID;
-
+    REFERENCES public.public_user (id);
 
 ALTER TABLE IF EXISTS public.public_user_lesson
     ADD FOREIGN KEY (id_lesson)
-    REFERENCES public.lesson (id) MATCH SIMPLE
-    ON UPDATE NO ACTION
-    ON DELETE NO ACTION
-    NOT VALID;
+    REFERENCES public.lesson (id);
 
-
+-- FK lesson
 ALTER TABLE IF EXISTS public.lesson
     ADD FOREIGN KEY (id_course)
-    REFERENCES public.course (id) MATCH SIMPLE
-    ON UPDATE NO ACTION
-    ON DELETE NO ACTION
-    NOT VALID;
-
+    REFERENCES public.course (id);
 
 ALTER TABLE IF EXISTS public.lesson
     ADD FOREIGN KEY (id_next_lesson)
-    REFERENCES public.lesson (id) MATCH SIMPLE
-    ON UPDATE NO ACTION
-    ON DELETE NO ACTION
-    NOT VALID;
+    REFERENCES public.lesson (id);
 
-
+-- FK exercises
 ALTER TABLE IF EXISTS public.exercises
     ADD FOREIGN KEY (id_lesson)
-    REFERENCES public.lesson (id) MATCH SIMPLE
-    ON UPDATE NO ACTION
-    ON DELETE NO ACTION
-    NOT VALID;
+    REFERENCES public.lesson (id);
 
-
+-- FK option
 ALTER TABLE IF EXISTS public.option
     ADD FOREIGN KEY (id_excercise)
-    REFERENCES public.exercises (id) MATCH SIMPLE
-    ON UPDATE NO ACTION
-    ON DELETE NO ACTION
-    NOT VALID;
+    REFERENCES public.exercises (id);
 
-
+-- FK public_user_option
 ALTER TABLE IF EXISTS public.public_user_option
     ADD FOREIGN KEY (id_public_user)
-    REFERENCES public.public_user (id) MATCH SIMPLE
-    ON UPDATE NO ACTION
-    ON DELETE NO ACTION
-    NOT VALID;
-
+    REFERENCES public.public_user (id);
 
 ALTER TABLE IF EXISTS public.public_user_option
     ADD FOREIGN KEY (id_option)
-    REFERENCES public.option (id) MATCH SIMPLE
-    ON UPDATE NO ACTION
-    ON DELETE NO ACTION
-    NOT VALID;
-
-END;
+    REFERENCES public.option (id);
---
+++ sql/init/01-public-user-data.sql
@@ -0,0 +1,2 @@
+INSERT INTO public.public_user (nickname) VALUES ('tumorenito19'); -- id=1
+INSERT INTO public.public_user (nickname) VALUES ('princesa');     -- id=2
