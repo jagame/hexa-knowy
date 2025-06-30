7f697d5af08205768f354504bd0b4a11af1d8d87
Sinnick29 <davidgilcampos@gmail.com>
2025-06-30 19:11:54 +0200

    including the menu component

--- server/src/main/java/com/knowy/server/controller/LessonController.java
+++ server/src/main/java/com/knowy/server/controller/LessonController.java
@@ -2,6 +2,7 @@ package com.knowy.server.controller;
 
 
 
+import com.knowy.server.controller.dto.CourseDTO;
 import com.knowy.server.controller.dto.LessonDTO;
 import com.knowy.server.controller.dto.LinksLessonDto;
 import org.springframework.stereotype.Controller;
@@ -21,12 +22,36 @@ public class LessonController {
 	@GetMapping("/lesson")
 	public String showLesson(Model model) {
 
-		LessonDTO lesson = new LessonDTO(1, "JavaScript moderno", LessonDTO.LessonStatus.BLOCKED, "https://media.licdn.com/.../image.jpg");
+		LessonDTO lesson = new LessonDTO(1, "JavaScript moderno", LessonDTO.LessonStatus.BLOCKED);
 		//This does not work and crashes the whole branch.
 		//LessonDTO lesson = new LessonDTO("JavaScript moderno", "100 horas", "https://media.licdn.com/dms/image/v2/D4D12AQG_8eaFpxIX8g/article-cover_image-shrink_600_2000/article-cover_image-shrink_600_2000/0/1685624189443?e=2147483647&v=beta&t=sYff1Zh5dsatOSYyUiMR8zwn3VqkG3x55bwFbEo5D-g", "#", "Teoria de la leccion");
 		model.addAttribute("lesson", lesson);
 
+		CourseDTO course = new CourseDTO();
+		course.setName("Java Básico");
+		course.setPercentageCompleted(40);
 
+		List<LessonDTO> lessons = List.of(
+			new LessonDTO(1, "Introducción", LessonDTO.LessonStatus.COMPLETE),
+			new LessonDTO(2, "¿Qué es Java y cómo funciona?", LessonDTO.LessonStatus.COMPLETE),
+			new LessonDTO(3, "Tipos de datos y variables", LessonDTO.LessonStatus.COMPLETE),
+			new LessonDTO(5, "Bucles", LessonDTO.LessonStatus.COMPLETE) ,
+			new LessonDTO(6, "Métodos (funciones)", LessonDTO.LessonStatus.COMPLETE),
+			new LessonDTO(7, "Arreglos (arrays)", LessonDTO.LessonStatus.NEXT_LESSON),
+			new LessonDTO(8, "Introducción a la Programación Orientada a Objetos", LessonDTO.LessonStatus.BLOCKED),
+			new LessonDTO(9, "Introducción a la Programación Orientada a Objetos", LessonDTO.LessonStatus.BLOCKED),
+			new LessonDTO(10, "Introducción a la Programación Orientada a Objetos", LessonDTO.LessonStatus.BLOCKED)
+		);
+
+		int lastLesson = -1;
+		for(int i = 0; i < lessons.size(); i++) {
+			if(lessons.get(i).getStatus() == LessonDTO.LessonStatus.COMPLETE) {
+				lastLesson = i;
+			}
+		}
+		course.setLessons(lessons);
+		model.addAttribute("course", course);
+		model.addAttribute("lastLesson", lastLesson);
 
 		// Array of links from each lesson?
 		List<LinksLessonDto> LinksLessonList = new ArrayList<>();
--- server/src/main/java/com/knowy/server/controller/dto/LessonDTO.java
+++ server/src/main/java/com/knowy/server/controller/dto/LessonDTO.java
@@ -2,16 +2,17 @@ package com.knowy.server.controller.dto;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @AllArgsConstructor
+@NoArgsConstructor
 @Getter
 @Setter
 public class LessonDTO {
 	private int number;
 	private String name;
 	private LessonStatus status; //depende de ENUM LessonStatus
-	private String image;
 
 	public enum LessonStatus {
 		COMPLETE,
