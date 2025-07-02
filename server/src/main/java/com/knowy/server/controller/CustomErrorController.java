5455c364dd9842e92a775e995d20068335166762
Aarón Helices Martín-Niño <aaronhelices@gmail.com>
2025-07-02 22:17:13 +0200

    Boceto de las páginas de errores añadidas

---
+++ server/src/main/java/com/knowy/server/controller/CustomErrorController.java
@@ -0,0 +1,32 @@
+package com.knowy.server.controller;
+
+import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.boot.web.servlet.error.ErrorController;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class CustomErrorController implements ErrorController {
+
+	@RequestMapping("/error")
+	public String handleError(HttpServletRequest request, Model model) {
+		Object status = request.getAttribute(RequestDispatcher.ERROR_STATUS_CODE);
+
+		if (status != null) {
+			int statusCode = Integer.parseInt(status.toString());
+			model.addAttribute("statusCode", statusCode); // se lo pasas al HTML
+
+			switch (statusCode) {
+				case 403: return "error/403";
+				case 404: return "error/404";
+				case 500: return "error/500";
+				default: return "error/error";
+			}
+		}
+
+		model.addAttribute("statusCode", "Desconocido");
+		return "error/error";
+	}
+}
