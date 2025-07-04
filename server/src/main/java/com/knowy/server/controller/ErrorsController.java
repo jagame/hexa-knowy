8616b0ff57f2f9952c1bc2683788072c169bb3fc
Aarón Helices Martín-Niño <aaronhelices@gmail.com>
2025-07-04 19:17:18 +0200

    Cambios para controlar bien los errores

--- server/src/main/java/com/knowy/server/controller/ErrorsController.java
+++ server/src/main/java/com/knowy/server/controller/ErrorsController.java
@@ -1,29 +1,54 @@
 package com.knowy.server.controller;
 
+import com.knowy.server.controller.dto.SessionUser;
+import jakarta.servlet.http.HttpSession;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.resource.NoResourceFoundException;
 
+import static com.knowy.server.controller.AccessController.SESSION_LOGGED_USER;
+
 @ControllerAdvice
 public class ErrorsController {
 
+	// Error 404 - Página no encontrada
 	@ExceptionHandler(NoResourceFoundException.class)
-	public ModelAndView handleNoHandlerFoundException(NoResourceFoundException ex) {
-		ModelAndView mv = new ModelAndView();
+	public ModelAndView handleNotFound(NoResourceFoundException ex, HttpSession session) {
+		ModelAndView mv = new ModelAndView("error/404");
 		mv.setStatus(HttpStatus.NOT_FOUND);
-		mv.setViewName("error/404");
-		mv.getModelMap().addAttribute("errorMessage", ex.getMessage());
+		mv.addObject("statusCode", 404);
+		mv.addObject("errorMessage", "La página que buscas no fue encontrada.");
+		addSessionUserAttributes(session, mv);
+		return mv;
+	}
+
+	@ExceptionHandler(IllegalArgumentException.class)
+	public ModelAndView handleBadRequest(IllegalArgumentException ex, HttpSession session) {
+		ModelAndView mv = new ModelAndView("error/error");
+		mv.setStatus(HttpStatus.BAD_REQUEST);
+		mv.addObject("statusCode", 400);
+		mv.addObject("errorMessage", ex.getMessage());
+		addSessionUserAttributes(session, mv);
 		return mv;
 	}
 
 	@ExceptionHandler(Exception.class)
-	public ModelAndView handleAllExceptions(Exception ex) {
-		ModelAndView mv = new ModelAndView();
+	public ModelAndView handleServerError(Exception ex, HttpSession session) {
+		ModelAndView mv = new ModelAndView("error/500");
 		mv.setStatus(HttpStatus.INTERNAL_SERVER_ERROR);
-		mv.setViewName("error/500");
-		mv.getModelMap().addAttribute("errorMessage", ex.getMessage());
+		mv.addObject("statusCode", 500);
+		mv.addObject("errorMessage", ex.getMessage());
+		addSessionUserAttributes(session, mv);
 		return mv;
+	}
+
+	private void addSessionUserAttributes(HttpSession session, ModelAndView mv) {
+		SessionUser loggedUser = (SessionUser) session.getAttribute(SESSION_LOGGED_USER);
+		if (loggedUser != null) {
+			mv.addObject("nickname", loggedUser.nickname());
+			mv.addObject("profileImageUrl", loggedUser.profileImageUrl());
+		}
 	}
 }
