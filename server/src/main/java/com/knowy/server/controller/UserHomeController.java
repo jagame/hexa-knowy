bf6472bd3fee282f4326363856e179c016e09be2
Aarón Helices Martín-Niño <aaronhelices@gmail.com>
2025-07-09 21:50:23 +0200

    Aplicando el arreglo del home y su nueva estructura del html (en construcción)

--- server/src/main/java/com/knowy/server/controller/UserHomeController.java
+++ server/src/main/java/com/knowy/server/controller/UserHomeController.java
@@ -73,6 +73,6 @@ public class UserHomeController {
 //		languageOptions.add(language3);
 //
 //		interfaceScreen.addAttribute("languageOptions", languageOptions);
-		return "pages/user-home";
+		return "pages/new-user-home";
 	}
 }
