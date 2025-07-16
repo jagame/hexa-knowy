a8ac6e413651a7319c3e250f855a077b39522c59
Sinnick29 <davidgilcampos@gmail.com>
2025-07-16 18:28:17 +0200

    Change KnowyValidationException to extend RuntimeException

--- server/src/main/java/com/knowy/server/controller/exception/KnowyValidationException.java
+++ server/src/main/java/com/knowy/server/controller/exception/KnowyValidationException.java
@@ -1,6 +1,6 @@
 package com.knowy.server.controller.exception;
 
-public class KnowyValidationException extends Exception {
+public class KnowyValidationException extends RuntimeException {
 	public KnowyValidationException(String message) {
 		super(message);
 	}
