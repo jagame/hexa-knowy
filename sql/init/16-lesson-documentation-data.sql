617f4e34c3bb13a5a8d56f586da8ad3ada232029
Aarón Helices Martín-Niño <aaronhelices@gmail.com>
2025-07-23 21:09:56 +0200

    Cambios en el diseño y bbdd arreglada más info añadida

--- sql/init/16-lesson-documentation-data.sql
+++ sql/init/16-lesson-documentation-data.sql
@@ -5,15 +5,18 @@ VALUES (1, 1);
 
 -- Lección 2
 INSERT INTO public.lesson_documentation (id_lesson, id_documentation)
-VALUES (2, 1);
+VALUES (2, 1),
+	   (2, 4);
 
 -- Lección 3
 INSERT INTO public.lesson_documentation (id_lesson, id_documentation)
-VALUES (3, 1);
+VALUES (3, 1),
+	   (3, 4);
 
 -- Lección 4
 INSERT INTO public.lesson_documentation (id_lesson, id_documentation)
-VALUES (4, 1);
+VALUES (4, 1),
+	   (4, 4);
 
 -- Lección 5
 INSERT INTO public.lesson_documentation (id_lesson, id_documentation)
@@ -30,12 +33,15 @@ VALUES (7, 1);
 
 -- Lección 8
 INSERT INTO public.lesson_documentation (id_lesson, id_documentation)
-VALUES (8, 1);
+VALUES (8, 1),
+	   (8, 6);
 
 -- Lección 9
 INSERT INTO public.lesson_documentation (id_lesson, id_documentation)
-VALUES (9, 1);
+VALUES (9, 1),
+	   (9, 6);
 
 -- Lección 10
 INSERT INTO public.lesson_documentation (id_lesson, id_documentation)
-VALUES (10, 1);
+VALUES (10, 1),
+	   (10, 3);
