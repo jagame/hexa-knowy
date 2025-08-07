37a591a5fbb1bb8b18fe33dd559b6604f09c6d96
saramforte <saraforte310@gmail.com>
2025-08-07 19:33:55 +0200

    Removed unused exceptions

--- server/src/main/java/com/knowy/server/infrastructure/controller/exception/NextLessonNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.knowy.server.infrastructure.controller.exception;
-
-public class NextLessonNotFoundException extends Exception {
-	public NextLessonNotFoundException(String message) {
-		super(message);
-	}
-}
--- server/src/main/java/com/knowy/server/infrastructure/controller/exception/NoCompletedLessonFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.knowy.server.infrastructure.controller.exception;
-
-public class NoCompletedLessonFoundException extends Exception {
-	public NoCompletedLessonFoundException(String message) {
-		super(message);
-	}
-}
